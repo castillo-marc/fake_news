f956728de8b061b57daee8d34b27d46a2929121f
castillo-marc <marc.castillo@berkeley.edu>
2022-09-23 10:43:39 -0700

    Still trying to reduce overall size

--- process_text.py
+++ process_text.py
@@ -1,26 +1,27 @@
-from bs4 import BeautifulSoup
-import requests
-import re
+# from bs4 import BeautifulSoup
+# import requests
+# import re
 
 
-def scraper(url):
-    try:
-        content = requests.get(url, timeout=30)
-        soup = BeautifulSoup(content.text, 'html.parser')
+# def scraper(url):
+#     try:
+#         content = requests.get(url, timeout=30)
+#         soup = BeautifulSoup(content.text, 'html.parser')
 
-        text = soup.findAll('p')
-        text = [word.text for word in text]
-        text = ' '.join(text)
-        text = re.sub('\W+', ' ', re.sub('xa0', ' ', text))
-    except: 
-         print("Couldn't parse website")
+#         text = soup.findAll('p')
+#         text = [word.text for word in text]
+#         text = ' '.join(text)
+#         text = re.sub('\W+', ' ', re.sub('xa0', ' ', text))
+#     except: 
+#          print("Couldn't parse website")
 
-    return text
+#     return text
 
+# commenting out everything aside from clean_text
 def clean_text(text):
     text = re.sub("[^a-zA-Z]", " ", text.lower())
     return text
 
-if __name__ == "__main__":
-    scraped = scraper("https://www.frontiersin.org/articles/10.3389/fpsyg.2020.566790/full")
-    print(clean_text(scraped))
+# if __name__ == "__main__":
+#     scraped = scraper("https://www.frontiersin.org/articles/10.3389/fpsyg.2020.566790/full")
+#     print(clean_text(scraped))
