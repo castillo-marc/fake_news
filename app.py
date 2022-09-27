7e3993ae375e67d02b0f5275fb5c7465afb828d2
castillo-marc <marc.castillo@berkeley.edu>
2022-09-26 17:31:30 -0700

    Fix config support

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ from sentence_transformers import SentenceTransformer
 app = Flask(__name__)
 
 env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
+app.config.from_object(env_config)
 
 model_path = "models/"
 
